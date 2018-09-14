f538eb4dcd4c9d8a15d11eb917b6f7fbfc559aaf
TopMak <26412430+TopMak@users.noreply.github.com>
2018-09-14 15:12:01 +0300

    Teht6.6 add redux connect to notification

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,41 +1,64 @@
 import React from 'react'
 import { clearNotification } from '../reducers/notificationReducer'
+import { connect } from 'react-redux'
 
-class Notification extends React.Component {
+// class Notification extends React.Component {
+
+const Notification = (props) => {
 
   //Notification "automatically resets" (no need to call every time notification is set)
-  notificationTimeout(){
-    setTimeout(() => {
-      console.log('timeout')
-      this.props.store.dispatch(clearNotification())
-    }, 5000)
+  // notificationTimeout(){
+  //   //clearTimeout(timerHandle)
+  //   setTimeout(() => {
+  //     console.log('timeout')
+  //     this.props.clearCurrentNotification()
+  //   }, 5000)
+  // }
+
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
   }
 
-  render() {
+  if(props.notification === ''){
+    return (
+      <div>
+        {/*No notification */}
+      </div>
+    )
+  } else {
 
-    const style = {
-      border: 'solid',
-      padding: 10,
-      borderWidth: 1
-    }
+    return (
+      <div style={style}>
+        {props.notification}
+        {props.clearCurrentNotification()}
+      </div>
+    )
 
-    if(this.props.store.getState().notification === ''){
-      return (
-        <div>
-          {/*No notification */}
-        </div>
-      )
-    } else {
+  }
 
-      return (
-        <div style={style}>
-          {this.props.store.getState().notification}
-          {this.notificationTimeout()}
-        </div>
-      )
+}
 
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    clearCurrentNotification: () => {
+      setTimeout(() => {
+        console.log('timeout')
+        dispatch(clearNotification())
+      }, 5000)
     }
   }
 }
 
-export default Notification
+// export default Notification
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Notification)
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import store from './store'
+import { Provider } from 'react-redux'
 
 
 const render = () => {
+  //annetaan store providen ja suoraan APP:ille propsina (muutetaan ensin vain Notification (teht6.6))
   ReactDOM.render(
-    <App store={store} />,
+    <Provider store={store}>
+      <App store={store}/>
+    </Provider>,
     document.getElementById('root')
   )
 }
