294a2ee4931e917c454b5264ae4101f5691cf85d
TopMak <26412430+TopMak@users.noreply.github.com>
2018-09-21 12:26:57 +0300

    Teht6.10 add backend mockup service

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 
+import { connect } from 'react-redux'
+import { initAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
+
 class App extends React.Component {
+
+  componentDidMount = async () => {
+    const anecdotes = await anecdoteService.getAll()
+    this.props.initAnecdotes(anecdotes)
+  }
 
   render() {
     //const anecdotes = this.props.store.getState()
@@ -20,4 +29,7 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default connect(
+  null,
+  { initAnecdotes }
+)(App)
