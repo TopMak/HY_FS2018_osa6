00f610fafcad52d80ff188ab231ff2506311434c
TopMak <26412430+TopMak@users.noreply.github.com>
2018-09-27 15:52:02 +0300

    teht6.16 add router and paths+menu, refactoring components too

---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+const Menu = () => {
+
+  return (
+    <div>
+      <Link to="/">anecdotes</Link>&nbsp;
+      <Link to="/create">create new</Link> &nbsp;
+      <Link to="/about">about</Link> &nbsp;
+    </div>
+  )
+}
+
+export default Menu
